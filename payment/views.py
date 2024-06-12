aaa44f15bf2e2fa8dcce379a1ed18bfd9ad12221
Kalkidan Amare <kalkidanamare11a@gmail.com>
2024-06-13 01:58:23 +0300

    retrieving user data to auto fill the payment form

--- payment/views.py
+++ payment/views.py
@@ -1,27 +1,33 @@
-from django.shortcuts import redirect, render,HttpResponse
-from .chapa import payment
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render, HttpResponse
 from django.contrib import messages
 from django.template import loader
-# Create your views here.
+from file.models import Course, PurchasedCourse
+from .chapa import payment
+
+@login_required
 def pay(request):
     if request.method == 'POST':
-        # Get form data
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
+        course_name = request.POST.get('course_name')
+        course = Course.objects.get(name=course_name)
+
+        # Retrieving user information from the logged-in user
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+        email = request.user.email
         phone = request.POST.get('phone')
-        amount = request.POST.get('amount')
-        email = request.POST.get('email')
-        
+        amount = course.price
+
+        # Proceed with payment processing
         ret = payment(first_name, last_name, phone, amount, email)
-        # print(ret)
         if ret['status'] == 'success':
-            checkout_url = ret['data']['checkout_url']
-       
-            return HttpResponse(loader.render_to_string('new_tab.html', {'checkout_url': checkout_url}))
+            PurchasedCourse.objects.create(user=request.user, course=course)
+            return redirect('chapa') 
         else:
             error_message = ret['message']
             messages.error(request, error_message)
-        # return render(request, 'success.html')
-    
-    # If GET request, render the payment form
-    return render(request, 'pay.html')
+
+    # If GET request, render the pay.html template
+    course_name = request.GET.get('course_name')
+    course = Course.objects.get(name=course_name)
+    return render(request, 'pay.html', {'course': course, 'user': request.user})
