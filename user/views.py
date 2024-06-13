e4576974fd5e88885a86111275043819562d4d1f
KidusHawoltu <kidushawoltu@gmail.com>
2024-06-13 09:01:21 +0300

    updated user

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterUser
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login,logout
 
 
@@ -13,9 +14,21 @@ def sign_up(request):
     elif request.method == 'GET':
         form = RegisterUser()
 
-        return render(request, 'registration/signup.html',{'form':form})
+    return render(request, 'registration/signup.html',{'form':form})
     
 
 def log_out(request):
     logout(request)
-    return redirect('/login')
+    return redirect('homepage')
+
+# def log_in(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('/home')
+#     else:
+#         form = AuthenticationForm()
+    
+#     return render(request, 'registration/login.html', {'form': form})
